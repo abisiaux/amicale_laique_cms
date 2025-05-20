7537633d8a23ea4278b62d5f210f2d6159975ca7
Alexandre Bisiaux <alexandre_bisiaux@connect-tech.sncf>
2025-05-20 16:51:11 +0200

    push schema

---
+++ src/api/proces-verbal/controllers/proces-verbal.ts
@@ -0,0 +1,7 @@
+/**
+ * proces-verbal controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::proces-verbal.proces-verbal');
---
+++ src/api/proces-verbal/routes/proces-verbal.ts
@@ -0,0 +1,7 @@
+/**
+ * proces-verbal router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::proces-verbal.proces-verbal');
