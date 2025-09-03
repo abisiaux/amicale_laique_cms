274568024ec161e3cb09f069c45ddea0b9beaea5
Alexandre Bisiaux <alexandre_bisiaux@connect-tech.sncf>
2025-09-03 15:20:02 +0200

    Revert "add wepb converter"
    This reverts commit d8ef8fd495681c5e1a804979a7f8434fd44af9c8.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,1 @@
-export default () => ({
-  // ...
-  "webp-converter": {
-    enabled: true,
-    config: {
-      // mimeTypes that converts to WebP. Default is ['image/png', 'image/jpeg', 'image/jpg']
-      mimeTypes: undefined,
-      options: {
-        // WebP options: https://sharp.pixelplumbing.com/api-output#webp
-      },
-    },
-  },
-});
+export default () => ({});
