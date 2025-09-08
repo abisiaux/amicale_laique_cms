4cc8f6ab3128204cb79a17c25e713320eaa644e5
Alexandre Bisiaux <alexandre_bisiaux@connect-tech.sncf>
2025-09-08 11:27:59 +0200

    add cache control

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,16 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "global::cache-control", // <- correspond à src/middlewares/cache-control.ts
+    config: {},
+  },
 ];
---
+++ src/middlewares/cache-control.ts
@@ -0,0 +1,29 @@
+export default () => {
+  return async (ctx, next) => {
+    await next();
+
+    if (ctx.request.url.startsWith("/api")) {
+      if (ctx.url.startsWith("/api/proces-verbaux")) {
+        ctx.set("Cache-Control", "public, max-age=3600"); // 1 heure
+      }
+      if (ctx.url.startsWith("/api/evenements")) {
+        ctx.set("Cache-Control", "public, max-age=300"); // 5 minutes
+      }
+      if (ctx.url.startsWith("/api/membres")) {
+        ctx.set("Cache-Control", "public, max-age=86400"); // 24 heures
+      }
+      if (ctx.url.startsWith("/api/actualites/")) {
+        ctx.set("Cache-Control", "public, max-age=900"); // 15 minutes
+      }
+      if (ctx.url.startsWith("/api/actualites")) {
+        ctx.set("Cache-Control", "public, max-age=300"); // 5 minutes
+      }
+      if (ctx.url.startsWith("/api/services/")) {
+        ctx.set("Cache-Control", "public, max-age=900"); // 15 minutes
+      }
+      if (ctx.url.startsWith("/api/services")) {
+        ctx.set("Cache-Control", "public, max-age=300"); // 5 minutes
+      }
+    }
+  };
+};
