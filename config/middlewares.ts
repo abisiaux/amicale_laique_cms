43c73546712ba5a6ebb6dee000982a4d6ed94033
Alexandre BISIAUX <alexandre_bisiaux@connect-tech.sncf>
2025-12-16 16:33:22 +0100

    add cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,48 @@
 export default [
-  "strapi::logger",
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-  {
-    name: "global::cache-control", // <- correspond à src/middlewares/cache-control.ts
-    config: {},
-  },
+    "strapi::logger",
+    "strapi::errors",
+    {
+        name: 'strapi::security',
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'connect-src': [
+                        "'self'",
+                        'https://api.aljvligne.fr',
+                    ],
+                },
+            },
+        },
+    },
+    {
+        name: "strapi::cors",
+        config: {
+            enabled: true,
+            origin: [
+                "https://aljvligne.fr",
+                "https://www.aljvligne.fr",
+                "https://api.aljvligne.fr",
+            ],
+            headers: [
+                "Content-Type",
+                "Authorization",
+                "Origin",
+                "Accept",
+            ],
+            methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+            keepHeaderOnError: true,
+            credentials: false,
+        },
+    },
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+    {
+        name: "global::cache-control", // <- correspond à src/middlewares/cache-control.ts
+        config: {},
+    },
 ];
